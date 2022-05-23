e051b795b0aad5864ee22070308e1cec5af44b13
Chakorn Panasutakarn <chakornotaku@gmail.com>
2022-05-23 09:13:08 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dockerVps-letEncrypt
+ 
---
+++ website2_chakorn/html/index.php
@@ -0,0 +1,23 @@
+<?php
+    echo "Hello : from https://chakorn2.me.ipv9.xyz/ <br>";
+    echo "Connected database server<br>";
+    echo "Selected database";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LEMP STACK DEMO</title>
+</head>
+<body>
+    <h1>LEMP STACK DEMO</h1>
+    <a href="http://chakorn.phpmyadmin.me.ipv9.xyz">CLICK TO PHPMYADMIN</a> <br>
+    <a href="http://chakorn.me.ipv9.xyz">CLICK TO Chakorn.me</a>
+
+    <?php phpinfo(); ?>
+</body>
+
+</html>
